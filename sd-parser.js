ad5f087ae34995f83b362a4a2e311f506df798cb
RichardRain <alexander.hell.scorpion@gmail.com>
2023-03-11 13:24:13 +0300

    change parse

--- sd-parser.js
+++ sd-parser.js
@@ -1,13 +1,13 @@
+const { PNGMetadata } = require("./image_metadata");
+
 var originalText;
 var doneText;
 
 function parseText() {
 	originalText = document.getElementById("parser").value;
 	originalText = "Prompt: " + originalText;
-	originalText = originalText.replace("&#xA;-", " \n\nNegative: ");
-	originalText = originalText.replace("&lt;", "<");
-	originalText = originalText.replace("&gt;", ">");
-	originalText = originalText.replace("&#xA;", " \n\n");
+	originalText = originalText.replace("-", " \nNegative: ");
+	originalText = originalText.replace("Steps:", " \nSteps:");
 
 	document.getElementById("done").value = originalText;
 	document.getElementById("copyButton").innerText = "Copy!";
